27c5d89542617302873de44c0e3dfddc940cd88c
Paul Mooney <paul.mooney@hmhco.com>
2015-02-27 11:54:47 +0000

    Deleted mockRobotPart

--- tests/daishi/tutorials/mockedRobotPart.java
+++
@@ -1,8 +0,0 @@
-package daishi.tutorials;
-
-public class mockedRobotPart extends robotPart {
-
-    public mockedRobotPart(robotPartCategory robotPartCategory) {
-        super(robotPartCategory);
-    }
-}
